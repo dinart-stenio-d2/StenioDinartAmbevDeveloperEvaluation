b565b557ca29add43362bffffa179d9afd4471d9
dinart-stenio-d2 <124164706+dinart-stenio-d2@users.noreply.github.com>
2024-12-06 10:13:24 -0300

    Created Sales aggregation root and its discounts business rule

---
+++ src/Ambev.DeveloperEvaluation.Common/SharedKernel/IAggregateRoot.cs
@@ -0,0 +1,6 @@
+﻿namespace Ambev.DeveloperEvaluation.Common.SharedKernel
+{
+    public interface IAggregateRoot
+    {
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Domain/Services/SaleService.cs
@@ -0,0 +1,33 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Services.Interfaces;
+
+namespace Ambev.DeveloperEvaluation.Domain.Services
+{
+    public class SaleService : ISaleService
+    {
+        public void ApplySalesDiscountRules(Sale sale)
+        {
+            foreach (var item in sale.Items)
+            {
+                if (item.Quantity < 4)
+                {
+                    item.Discount = 0;
+                }
+                else if (item.Quantity >= 4 && item.Quantity < 10)
+                {
+                    item.Discount = 10;
+                }
+                else if (item.Quantity >= 10 && item.Quantity <= 20)
+                {
+                    item.Discount = 20;
+                }
+                else
+                {
+                    //TODO : Apply fluent validations rules here 
+                    throw new InvalidOperationException("Cannot sell more than 20 items of the same product.");
+                }
+            }
+            sale.CalculateTotalAmount();
+        }
+    }
+}
